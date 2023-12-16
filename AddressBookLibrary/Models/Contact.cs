6bd3260a75fec3ebf9b08ce1d2c04bf72b3686cb
Farangis Ölund <bless.faro@gmail.com>
2023-12-16 11:46:42 +0100

    Add project files.

---
+++ AddressBookLibrary/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using AddressBookLibrary.Interfaces;
+
+namespace AddressBookLibrary.Models;
+
+public class Contact : IContact
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Address { get; set; } = null!;
+}
