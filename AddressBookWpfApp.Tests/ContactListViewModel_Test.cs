ecc6663e748ad20a6be1614723933ecf1a483968
Farangis Ölund <bless.faro@gmail.com>
2023-12-17 22:04:29 +0100

    Test for project wpf AddToContacList cterated

--- AddressBookWpfApp.Tests/ContactListViewModel_Test.cs
+++ AddressBookWpfApp.Tests/ContactListViewModel_Test.cs
@@ -2,13 +2,47 @@
 using AddressBookLibrary.Interfaces;
 using AddressBookLibrary.Models;
 using AddressBookWpfApp.ViewModels;
-using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using System.Collections.ObjectModel;
 
 namespace AddressBookWpfApp.Tests;
 
 public class ContactListViewModel_Test
 {
+    [Fact]
+    public async Task AddContactToList_WhenValidContactInfo_AddsContactToList()
+    {
+        // Arrange
+        var contactServiceMock = new Mock<IContactService>();
+        var serviceProviderMock = new Mock<IServiceProvider>();
+
+        var contactList = new ObservableCollection<Contact>();
+
+        var viewModel = new ContactAddViewModel(serviceProviderMock.Object, contactServiceMock.Object, contactList);
+
+        viewModel.Contact.FirstName = "Elsa";
+        viewModel.Contact.LastName = "Olund";
+        viewModel.Contact.Email = "elsa@example.com";
+
+        // Set up mock behavior for AddContactAsync
+        contactServiceMock.Setup(x => x.AddContactAsync(It.IsAny<Contact>()))
+                          .ReturnsAsync(true)
+                          .Verifiable();
+
+        // Set up mock behavior for GetAllContactsAsync
+        contactServiceMock.Setup(x => x.GetAllContactsAsync())
+                          .ReturnsAsync(new List<Contact>()) 
+                          .Verifiable();
+
+        // Act
+        await viewModel.AddContactToList();
+
+        // Assert
+        contactServiceMock.Verify(x => x.AddContactAsync(It.IsAny<Contact>()), Times.Once);
+        contactServiceMock.Verify(x => x.GetAllContactsAsync(), Times.Once);
+
+    }
+
     [Fact]
     public async Task LoadContacts_ShouldRetrieveContactsAndUpdateContactList()
     {
